1947d0563a5a4afe387b622edb5bdad1df06c1e4
burzykovski <burzyk.mb@gmail.com>
2018-01-21 18:09:03 +0100

    create basic Navigation component | two versions whether user is logged in or not | click on Logowanie or Wyloguj changes the state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React, { Component } from "react";
 import { Grid } from "react-bootstrap";
 import "./App.css";
 
+import Navigation from "./components/Navigation";
+
 class App extends Component {
+  state = {
+    userlogged: true
+  }
+
+  handleLogging = () => {
+    if(this.state.userlogged) {
+      this.setState({userlogged: false})
+    } else {
+      this.setState({userlogged: true})
+    }
+  }
+
   render() {
-    return <div>Hello my friend</div>;
+    return (
+      <Navigation userlogged={this.state.userlogged} handleLogging={this.handleLogging} />
+    )
   }
 }
 
