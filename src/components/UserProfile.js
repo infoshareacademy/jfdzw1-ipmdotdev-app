98704f5843f63cf82753644e0bde5fc6dc72eb9f
piotryfon <pit.pit@wp.pl>
2018-02-21 18:47:27 +0100

    poprawa grid w UserProf

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -9,60 +9,80 @@ class UserProfile extends Component {
     return (
         <Grid>
           <Row className="show-grid">
-            <Col xs={6} md={4}>
-              <div style={{height: 500}}>
+            <Col xs={12} sm={3}>
+              <div style={{height: 510}}>
                 <img src={"http://via.placeholder.com/150x200"} width={150} height={200}/>
                 <h2>Piotr Redux</h2>
                   <h3>O mnie</h3>
-                  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est.</p>
+                  <p style={{textAlign: "left"}}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est.</p>
                 <p>Warszawa</p>
                   <p>Mężczyza</p>
                   <p>30 lat</p>
               </div>
             </Col>
-            <Col xs={8} md={8}>
+            <Col xs={12} sm={9}>
               <h2>Aktywne wyzwania:</h2>
             </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://via.placeholder.com/350x150" alt="242x200">
-                <h3>Thumbnail label</h3>
+            <Col xs={6} sm={3}>
+              <Thumbnail src="http://via.placeholder.com/300x150" alt="242x200">
+                <h4>Wyzwanie</h4>
                 <p>Description</p>
                 <p>
                   <Button bsStyle="primary">dodaj</Button>&nbsp;
-                  <Button bsStyle="default">zrezygnuj</Button>
+                  <Button bsStyle="danger">zrezygnuj</Button>
                 </p>
               </Thumbnail>
             </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://via.placeholder.com/350x150" alt="242x200">
-                <h3>Thumbnail label</h3>
+            <Col xs={6} sm={3}>
+              <Thumbnail src="http://via.placeholder.com/300x150" alt="242x200">
+                <h4>Wyzwanie</h4>
                 <p>Description</p>
                 <p>
                   <Button bsStyle="primary">dodaj</Button>&nbsp;
-                  <Button bsStyle="default">zrezygnuj</Button>
+                  <Button bsStyle="danger">zrezygnuj</Button>
                 </p>
               </Thumbnail>
             </Col>
-            <Col xs={8} md={8}>
+            <Col xs={6} sm={3}>
+              <Thumbnail src="http://via.placeholder.com/300x150" alt="242x200">
+                <h4>Wyzwanie</h4>
+                <p>Description</p>
+                <p>
+                  <Button bsStyle="primary">dodaj</Button>&nbsp;
+                  <Button bsStyle="danger">zrezygnuj</Button>
+                </p>
+              </Thumbnail>
+            </Col>
+            <Col xs={12} sm={9}>
               <h2>Propozycje wyzwań:</h2>
             </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://via.placeholder.com/350x150" alt="242x200">
-                <h3>Thumbnail label</h3>
+            <Col xs={6} sm={3}>
+              <Thumbnail src="http://via.placeholder.com/300x150" alt="242x200">
+                <h4>Wyzwanie</h4>
                 <p>Description</p>
                 <p>
                   <Button bsStyle="primary">dodaj</Button>&nbsp;
-                  <Button bsStyle="default">zrezygnuj</Button>
+                  <Button bsStyle="danger">zrezygnuj</Button>
                 </p>
               </Thumbnail>
             </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://via.placeholder.com/350x150" alt="242x200">
-                <h3>Thumbnail label</h3>
+            <Col xs={6} sm={3}>
+              <Thumbnail src="http://via.placeholder.com/300x150" alt="242x200">
+                <h4>Wyzwanie</h4>
                 <p>Description</p>
                 <p>
                   <Button bsStyle="primary">dodaj</Button>&nbsp;
-                  <Button bsStyle="default">zrezygnuj</Button>
+                  <Button bsStyle="danger">zrezygnuj</Button>
+                </p>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} sm={3}>
+              <Thumbnail src="http://via.placeholder.com/300x150" alt="242x200">
+                <h4>Wyzwanie</h4>
+                <p>Description</p>
+                <p>
+                  <Button bsStyle="primary">dodaj</Button>&nbsp;
+                  <Button bsStyle="danger">zrezygnuj</Button>
                 </p>
               </Thumbnail>
             </Col>
