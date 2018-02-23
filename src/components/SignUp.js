3520a6a2bd0be358fd832b4edf89fb01023fc6b6
piotryfon <pit.pit@wp.pl>
2018-02-23 23:31:33 +0100

    SignUp component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { Radio, FormControl, FormGroup, ControlLabel, Button, HelpBlock } from "react-bootstrap"
+import {Radio, FormControl, FormGroup, ControlLabel, Button, HelpBlock} from "react-bootstrap"
+
 
 const SignUp = () => {
-    function FieldGroup({ id, label, help, ...props }) {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const registerInput = {
+            login: this.login.value,
+            password: this.password.value,
+            name: this.name.value,
+            email: this.email.value,
+            gender: this.gender.value,
+            localization: {
+                country: this.country.value,
+                city: this.city.value
+            },
+            age: this.age.value,
+            textarea: this.textarea.value,
+            avatar: this.avatar.value
+        };
+
+        registerUser(registerInput);
+
+
+    }
+
+    const registerUser = registerInput => {
+
+        fetch("http://api.isa-jfdzw1.vipserv.org/ipmdev/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(registerInput)
+        })
+
+    };
+
+    function FieldGroup({id, label, help, ...props}) {
         return (
             <FormGroup controlId={id}>
                 <ControlLabel>{label}</ControlLabel>
@@ -11,40 +47,68 @@ const SignUp = () => {
             </FormGroup>
         );
     }
+
     const formInstance = (
-        <form>
+        <form onSubmit={handleSubmit}>
+
+            <FieldGroup id="formControlsLogin" label="Login" type="text" inputRef={input => {
+                this.login = input;
+            }}/>
+
             <FieldGroup
                 id="formControlsEmail"
                 type="email"
                 label="Email address"
                 placeholder="Enter email"
+                inputRef={input => {
+                    this.email = input;
+                }}
             />
-            <FieldGroup id="formControlsPassword" label="Password" type="password" />
-            <FieldGroup id="formControlsPassword" label="Confirm Password" type="password" />
+            <FieldGroup id="formControlsPassword" label="Password" type="password" inputRef={input => {
+                this.password = input;
+            }}/>
 
-            <FormGroup>
-                <Radio name="radioGroup" inline>
-                    female
-                </Radio>{' '}
-                <Radio name="radioGroup" inline>
-                    male
-                </Radio>{' '}
-                <FieldGroup id="age" label="age" type="number" ref={node => this.input = node} />
-                <FieldGroup id="country" label="Country" type="string" />
-                <FieldGroup id="city" label="City" type="string" />
-                <FormGroup controlId="formControlsSelectMultiple">
-                    <ControlLabel>Challenge</ControlLabel>
-                    <FormControl componentClass="select" multiple>
-                        <option value="select">zjedz snickersa</option>
-                        <option value="other">zrób 100 pompek</option>
-                        <option value="other">zarób 100 $</option>
-                        <option value="other">napisz test na 100%</option>
-                    </FormControl>
-                </FormGroup>
+            <FormGroup controlId="formControlsSelect">
+                <ControlLabel>Płeć</ControlLabel>
+                <FormControl
+                    componentClass="select"
+                    placeholder="Wybierz płeć"
+                    inputRef={input => {
+                        this.gender = input;
+                    }}
+                >
+                    <option value="male">Mężczyzna</option>
+                    <option value="female">Kobieta</option>
+                </FormControl>
             </FormGroup>
+
+            <FieldGroup id="country" label="Country" type="string" inline inputRef={input => {
+                this.country = input;
+            }}/>
+            <FieldGroup id="city" label="City" type="string" inline inputRef={input => {
+                this.city = input;
+            }}/>
+            <FieldGroup id="age" label="age" type="number" inline inputRef={input => {
+                this.age = input;
+            }}/>
+            <FormGroup controlId="formControlsTextarea" label="textarea" type="text">
+                <ControlLabel>Textarea</ControlLabel>
+                <FormControl componentClass="textarea" placeholder="textarea" inline inputRef={input => {
+                    this.textarea = input;
+                }}/>
+            </FormGroup>
+
+            <FieldGroup id="avatar" label="avatar" type="string" inline inputRef={input => {
+                this.avatar = input;
+            }}/>
+            <FieldGroup id="name" label="name" type="string" inline inputRef={input => {
+                this.name = input;
+            }}/>
             <Button type="submit">Submit</Button>
         </form>
     );
-   return formInstance;
+    return formInstance;
+
 };
+;
 export {SignUp};
